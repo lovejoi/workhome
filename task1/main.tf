2c39b66d4ffb8c9f8d762eb09bb53cfb3dcb9165
lovejoi <31632704+lovejoi@users.noreply.github.com>
2024-10-15 14:16:58 +0400

    update file

--- task1/main.tf
+++ task1/main.tf
@@ -1,6 +1,75 @@
-provider "aws" {
-  region = var.region
+terraform {
+  required_version = ">= 1.4.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.23.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.11.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+  }
 }
+
+
+variable "vpc_id" {
+  description = "The VPC ID where the EKS cluster will be created."
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "The list of subnet IDs for the EKS cluster."
+  type        = list(string)
+}
+
+variable "cluster_name" {
+  description = "The name of the EKS cluster."
+  type        = string
+}
+
+
+locals {
+  tags = {
+    Environment = "dev"  # You can customize this as needed
+    Project     = var.cluster_name
+  }
+}
+
+
+provider "aws" {
+  region  = "us-west-2"
+  profile = "default"
+}
+
+
+
+provider "aws" {
+  alias   = "us-east-1"
+  region  = "us-east-1"
+  profile = "default" 
+}
+
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
+
 
 provider "helm" {
   kubernetes {
@@ -8,30 +77,31 @@ provider "helm" {
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
       command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
     }
   }
 }
+
 
 provider "kubectl" {
   apply_retry_count      = 5
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   load_config_file       = false
-
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
     command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", "us-west-2"]
   }
 }
 
-data "aws_vpc" "existing" {
-  id = var.vpc_id
+
+data "aws_ecrpublic_authorization_token" "token" {
+  provider = aws.us-east-1
 }
 
-# IAM role for EKS cluster
+
 resource "aws_iam_role" "eks_cluster_role" {
   name = "${var.cluster_name}-cluster-role"
 
@@ -54,7 +124,6 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   role       = aws_iam_role.eks_cluster_role.name
 }
 
-# IAM role for EKS node group
 resource "aws_iam_role" "eks_node_group_role" {
   name = "${var.cluster_name}-node-group-role"
 
@@ -87,6 +156,7 @@ resource "aws_iam_role_policy_attachment" "eks_container_registry_readonly" {
   role       = aws_iam_role.eks_node_group_role.name
 }
 
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 19.0"
@@ -107,60 +177,41 @@ module "eks" {
       max_size       = 5
       desired_size   = 2
       iam_role_arn   = aws_iam_role.eks_node_group_role.arn
-
-      # Enable auto-scaling
-      create_schedule = true
-
-      schedules = {
-        scale-up = {
-          min_size     = 2
-          max_size     = 5
-          desired_size = 3
-          recurrence   = "0 7 * * MON-FRI"
-          time_zone    = "UTC"
-        }
-        scale-down = {
-          min_size     = 1
-          max_size     = 3
-          desired_size = 1
-          recurrence   = "0 19 * * MON-FRI"
-          time_zone    = "UTC"
-        }
-      }
     }
   }
 
-  # Enable OIDC provider for the cluster
-  enable_irsa = true
-
-  # Enable CloudWatch monitoring
-  create_cloudwatch_log_group = true
-  cluster_enabled_log_types   = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+  enable_irsa                   = true
+  create_cloudwatch_log_group   = true
+  cluster_enabled_log_types     = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
 }
+
 
 module "karpenter" {
   source  = "terraform-aws-modules/eks/aws//modules/karpenter"
-  version = "~> 19.0"
+  version = "19.20.0"
 
-  cluster_name = module.eks.cluster_name
-
+  cluster_name                    = module.eks.cluster_name
   irsa_oidc_provider_arn          = module.eks.oidc_provider_arn
   irsa_namespace_service_accounts = ["karpenter:karpenter"]
+
+  create_iam_role      = false
+  iam_role_arn         = aws_iam_role.eks_node_group_role.arn
+  irsa_use_name_prefix = false
+
+  tags = local.tags
 }
+
 
 resource "helm_release" "karpenter" {
   namespace        = "karpenter"
   create_namespace = true
 
-  name       = "karpenter"
-  repository = "oci://public.ecr.aws/karpenter"
-  chart      = "karpenter"
-  version    = "v0.27.5"
-
-  set {
-    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = module.karpenter.irsa_arn
-  }
+  name                = "karpenter"
+  repository          = "oci://public.ecr.aws/karpenter"
+  repository_username = data.aws_ecrpublic_authorization_token.token.user_name
+  repository_password = data.aws_ecrpublic_authorization_token.token.password
+  chart               = "karpenter"
+  version             = "v0.31.3"
 
   set {
     name  = "settings.aws.clusterName"
@@ -170,6 +221,11 @@ resource "helm_release" "karpenter" {
   set {
     name  = "settings.aws.clusterEndpoint"
     value = module.eks.cluster_endpoint
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.karpenter.irsa_arn
   }
 
   set {
@@ -183,26 +239,48 @@ resource "helm_release" "karpenter" {
   }
 }
 
+
 resource "kubectl_manifest" "karpenter_provisioner" {
   yaml_body = <<-YAML
-  apiVersion: karpenter.sh/v1alpha5
-  kind: Provisioner
-  metadata:
-    name: default
-  spec:
-    requirements:
-      - key: karpenter.sh/capacity-type
-        operator: In
-        values: ["spot", "on-demand"]
-      - key: kubernetes.io/arch
-        operator: In
-        values: ["amd64", "arm64"]
-    limits:
-      resources:
-        cpu: 1000
-    provider:
-      instanceTypes: ["t3.medium", "t3a.medium", "t4g.medium"]
-    ttlSecondsAfterEmpty: 30
+    apiVersion: karpenter.sh/v1alpha5
+    kind: Provisioner
+    metadata:
+      name: default
+    spec:
+      requirements:
+        - key: karpenter.sh/capacity-type
+          operator: In
+          values: ["on-demand"]
+        - key: "node.kubernetes.io/instance-type"
+          operator: In
+          values: ["c5.large","c5a.large", "c5ad.large", "c5d.large", "c6i.large", "t2.medium", "t3.medium", "t3a.medium"]
+      limits:
+        resources:
+          cpu: 1000
+      providerRef:
+        name: default
+      ttlSecondsAfterEmpty: 30
+  YAML
+
+  depends_on = [
+    helm_release.karpenter
+  ]
+}
+
+
+resource "kubectl_manifest" "karpenter_node_template" {
+  yaml_body = <<-YAML
+    apiVersion: karpenter.k8s.aws/v1alpha1
+    kind: AWSNodeTemplate
+    metadata:
+      name: default
+    spec:
+      subnetSelector:
+        karpenter.sh/discovery: ${module.eks.cluster_name}
+      securityGroupSelector:
+        karpenter.sh/discovery: ${module.eks.cluster_name}
+      tags:
+        karpenter.sh/discovery: ${module.eks.cluster_name}
   YAML
 
   depends_on = [
